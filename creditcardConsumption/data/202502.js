9213f2f78b37775b8ba6211a7974d739ce50a765
Kevin Chan <kevin.chan@nextbank.com.tw>
2025-03-05 14:43:15 +0800

    CreditcardConsumption init

---
+++ creditcardConsumption/data/202502.js
@@ -0,0 +1,266 @@
+window.consumptionData = {
+    '優食類': {
+        total: 7628,
+        details: [
+            '麵匡匡拉麵食堂 台北安居店 - 369元',
+            '金小噹韓式烤肉便當專賣店 - 433元',
+            '肯德基 KFC甜點專賣店 台北光復 - 291元',
+            '創義麵 松山店 - 437元',
+            '一沐日 臨江通化店 - 182元',
+            '小凱悅南村麵館 - 250元',
+            '古記 延三溫體牛 - 460元',
+            '格芮潘 義式創意廚房 - 300元',
+            '韓喜堂 - 331元',
+            '龜記茗品 - 160元',
+            '鮮乳 通化夜市店 - 286元',
+            '3733特大雞排 - 370元',
+            '韓勾a 韓式烤肉便當專賣店 - 563元',
+            '佐藤咖哩 總店 - 900元',
+            '拿坡里炸雞永吉店 - 661元',
+            '霏等茶 - 457元',
+            '勝口味大腸蚵仔麵線 大安店 - 452元',
+            'Mister Donut 松江南京 - 168元',
+            '2派克雞排 永吉店 - 210元',
+            '饒河夜市 巷口亭古早味甜品店 - 198元'
+        ]
+    },
+    '優步類': {
+        total: 340,
+        details: [
+            'Q2 Taxi車隊 - 190元',
+            '耐斯車隊 - 150元'
+        ]
+    },
+    '連加類': {
+        total: 999,
+        details: [
+            '林媽媽麻油山雞 - 140元',
+            '五高麵線麻辣鴨血 - 370元',
+            'CITYLINK - 90元',
+            '鮮茶道-北市通化 - 35元',
+            'UG TEA - 215元',
+            '躍獅連鎖藥局 - 149元'
+        ]
+    },
+    'OP錢包類': {
+        total: 436,
+        details: [
+            '統一超商 - 47元',
+            '統一超商 - 39元',
+            '統一超商 - 3元',
+            '統一超商 - 347元'
+        ]
+    },
+    '全盈支付類': {
+        total: 551,
+        details: [
+            '全家便利商店 - 55元',
+            '全家便利商店 - 35元',
+            '全家便利商店 - 55元',
+            '全家便利商店 - 49元',
+            '全家便利商店 - 13元',
+            '全家便利商店 - 45元',
+            '全家便利商店 - 45元',
+            '全家便利商店 - 80元',
+            '全家便利商店 - 59元',
+            '全家便利商店 - 26元',
+            '全家便利商店 - 34元'
+        ]
+    },
+    '家樂福類': {
+        total: 1426,
+        details: [
+            '台北松德店一般掃碼 - 618元',
+            '台北松德店一般掃碼 - 329元',
+            '台北松德店一般 - 479元'
+        ]
+    },
+    '美廉社類': {
+        total: 1054,
+        details: [
+            '信義虎林二店 - 187元',
+            '信義虎林二店 - 170元',
+            '信義虎林二店 - 139元',
+            '信義虎林二店 - 249元',
+            '信義虎林二店 - 309元'
+        ]
+    },
+    '百貨公司類': {
+        total: 1583,
+        details: [
+            'SHOPLINE-Havegreend - 1,295元',
+            '樂果合作有限公司 - 288元'
+        ]
+    },
+    '寵物類': {
+        total: 3951,
+        details: [
+            '寵物公園忠五店 - 980元',
+            '寵物公園忠五店 - 528元',
+            '寵物公園忠五店 - 2,443元'
+        ]
+    },
+    '餐飲類': {
+        total: 1773,
+        details: [
+            '必勝客 - 927元',
+            'Subway-敦化和平店 - 161元',
+            'Subway-松德餐廳 - 184元',
+            '安心食品服務(摩斯) - 165元',
+            '麥味登北市敦南遠企店 - 140元',
+            '台灣麥當勞SOK - 177元',
+            '萊爾富HiPay - 103元'
+        ]
+    },
+    '數位服務類': {
+        total: 4605,
+        details: [
+            'APPLE.COM/BILL - 90元',
+            'GOOGLE*YOUTUBEPREMIUM - 399元',
+            'Amazon web services - 15元',
+            'ANTHROPIC - 494元',
+            'APPLE.COM/BILL - 190元',
+            'APPLE.COM/BILL - 60元',
+            'KOBO SOFTWARE IRELAND - 192元',
+            'Pchome - 3,612元',
+            '藍新-面試趣 - 150元',
+            'IHERB退款 - -452元',
+            '國外交易服務費 - -145元'
+        ]
+    },
+    '醫療保健類': {
+        total: 480,
+        details: [
+            '臺北醫學大學附設醫院 - 450元',
+            '城市車旅北醫附醫停車場 - 30元'
+        ]
+    },
+    '生活用品類': {
+        total: 501,
+        details: [
+            '屈臣氏S0181松二店 - 501元'
+        ]
+    },
+    '住宿費': {
+        total: 1494,
+        details: [
+            '雲河概念旅館 - 1,494元'
+        ]
+    },
+    '通訊費': {
+        total: 807,
+        details: [
+            '信用卡扣繳中華電信費 - 807元'
+        ]
+    },
+    '悠遊卡加值': {
+        total: 1000,
+        details: [
+            '悠遊卡自動加值金額-台北捷運南港 - 500元',
+            '悠遊卡自動加值金額-美廉社 - 500元'
+        ]
+    },
+    '日本旅遊-住宿': {
+        total: 35135,
+        details: [
+            'BOOKING.COM - 7,647元 (含服務費114元)',
+            'BOOKING.COM - 5,906元 (含服務費88元)',
+            'APAHOTEL - 6,297元 (含服務費94元)',
+            'DISNEY HOTELS - 8,571元 (含服務費128元)',
+            'DISNEY HOTELS - 2,810元 (含服務費42元)',
+            '服務費 - 3,904元'
+        ]
+    },
+    '日本旅遊-交通': {
+        total: 7240,
+        details: [
+            'AIRPORTER - 2,343元 (含服務費35元)',
+            'UBER TRIP - 843元 (含服務費12元)',
+            'UBER TRIP - 651元 (含服務費9元)',
+            'UBER TRIP - 1,222元 (含服務費18元)',
+            'UBER TRIP - 1,121元 (含服務費16元)',
+            'JR EAST - 269元 (含服務費4元)',
+            'JR EAST - 176元 (含服務費2元)',
+            'JR EAST - 269元 (含服務費4元)',
+            'SUICA MOBILE PAYMENT - 436元 (含服務費6元)',
+            'SUICA MOBILE PAYMENT - 653元 (含服務費9元)',
+            '服務費 - -743元'
+        ]
+    },
+    '日本旅遊-購物': {
+        total: 29594,
+        details: [
+            'DAILY YAMAZAKI - 1,633元 (含服務費24元)',
+            'SUGI PHARMACY - 1,277元 (含服務費19元)',
+            'NARITA AIRPORT - 3,257元 (含服務費48元)',
+            'NARITA AIRPORT - 3,429元 (含服務費51元)',
+            'NARITA AIRPORT TERMINA - 2,829元 (含服務費42元)',
+            'NARITA AIRPORT - 225元 (含服務費3元)',
+            'GYOMUSUPAUENOHIROKOJIT - 803元 (含服務費12元)',
+            'DONQUIJOTE UENO - 11,977元 (含服務費179元)',
+            'TOKYO DISNEYRESORT購物 - 3,814元 (含服務費57元)',
+            '服務費 - 350元'
+        ]
+    },
+    '日本旅遊-餐飲': {
+        total: 11075,
+        details: [
+            'MATSUYA UENO - 216元 (含服務費3元)',
+            'KAWAKAMIAN - 1,596元 (含服務費23元)',
+            'DELICIA KARUIZAWATEN - 1,685元 (含服務費25元)',
+            'CHOUJIAN - 87元 (含服務費1元)',
+            'CHOUJIAN - 1,226元 (含服務費18元)',
+            'FUA-SUTOSHIYOTSUPUGURA - 104元 (含服務費1元)',
+            'FUA-SUTOSHIYOTSUPUGURA - 333元 (含服務費4元)',
+            'KARUIZAWA OYATSU - 423元 (含服務費6元)',
+            'OKAKIDOKOROTERAKOYAHOM - 933元 (含服務費13元)',
+            'DONGURIKYOUWAKOKU KARU - 406元 (含服務費6元)',
+            'RAKUTENPAY VEGANRAME - 1,757元 (含服務費25元)',
+            'SANDORATSUGU - 2,256元 (含服務費33元)',
+            '其他餐飲消費 - 882元 (含服務費13元)',
+            '服務費 - -829元'
+        ]
+    },
+    '日本旅遊-門票': {
+        total: 21921,
+        details: [
+            'karuizawa park - 2,170元 (含服務費32元)',
+            'TOKYO DISNEYRESORT - 521元 (含服務費7元)',
+            'TOKYO DISNEYRESORT - 3,992元 (含服務費59元)',
+            'TOKYO DISNEYRESORT - 2,162元 (含服務費32元)',
+            'TOKYO DISNEYRESORT - 2,162元 (含服務費32元)',
+            'TOKYO DISNEYRESORT - 1,729元 (含服務費25元)',
+            'TOKYO DISNEYRESORT - 2,162元 (含服務費32元)',
+            'TOKYO DISNEYRESORT - 1,427元 (含服務費21元)',
+            'TOKYO DISNEYRESORT - 367元 (含服務費5元)',
+            'TOKYO DISNEYRESORT - 735元 (含服務費11元)',
+            'TOKYO DISNEYRESORT - 1,262元 (含服務費18元)',
+            'TOKYO DISNEYRESORT - 3,242元 (含服務費48元)',
+            'TOKYO DISNEYRESORT - 346元 (含服務費5元)',
+            'TOKYO DISNEYRESORT - 1,204元 (含服務費18元)',
+            'TOKYO DISNEYRESORT - 1,859元 (含服務費27元)',
+            'TOKYO DISNEYRESORT - 2,400元 (含服務費36元)',
+            'TOKYO DISNEYRESORT退款 - -2,186元 (含服務費-32元)',
+            'TOKYO DISNEYRESORT退款 - -2,186元 (含服務費-32元)',
+            'TOKYO DISNEYRESORT退款 - -2,169元 (含服務費-32元)',
+            'YAMAOKA CLINIC - 1,620元 (含服務費24元)',
+            'MastercardTravelDisney退款 - -266元 (含服務費-3元)',
+            '服務費 - 1,438元'
+        ]
+    },
+    '旅遊住宿類': {
+        total: 9700,
+        details: [
+            '立吉富線上金流-奧丁丁旅行社 - 9,700元'
+        ]
+    },
+    '時尚購物類': {
+        total: 5099,
+        details: [
+            'Shein - 1,434元 (含服務費21元)',
+            'Shein - 1,876元 (含服務費28元)',
+            'Shein - 1,734元 (含服務費26元)',
+            '服務費 - 6元'
+        ]
+    }
+};
